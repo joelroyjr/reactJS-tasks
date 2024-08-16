c399b262842dc3ce9b39b2e8ab9ba5181d76c417
Joel Roy <jjoelroyr@gmail.com>
2024-08-16 21:11:38 +0530

    Add files via upload

---
+++ Task3/App.js
@@ -0,0 +1,18 @@
+
+// import logo from './logo.svg';
+import './style.css';
+
+import Task3 from './Components/Task3';
+
+function App() {
+  return (
+    
+   
+    <>
+    <Task3/>
+    
+    </>
+  );
+}
+
+export default App;
